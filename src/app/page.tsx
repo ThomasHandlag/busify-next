d9cf32825e907c0165c49d9a7080282519347055
unknown <truongthuongvan203@gmail.com>
2025-07-21 13:35:52 +0700

    chore: init busify-next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { toast, Toaster } from "sonner";
 
 export default function Home() {
   return (
@@ -82,22 +85,15 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          onClick={() => {
+            toast.info("Login button clicked");
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          Login
+        </Button>
       </footer>
+      <Toaster />
     </div>
   );
 }
