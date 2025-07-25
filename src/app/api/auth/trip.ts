10149394ee750651c3caaca39d6d7ccf29233e4c
Hoai Tino <hoaicoder0503@gmail.com>
2025-07-25 10:11:15 +0700

    feat: fetch data upcoming trips again

--- src/app/api/auth/trip.ts
+++ src/app/api/auth/trip.ts
@@ -5,7 +5,7 @@ export async function getUpcomingTrips(): Promise<Response> {
     const res = await api.get("api/trips/upcoming-trips");
     return res.data;
   } catch (error) {
-    console.error("Error fetching upcoming trips:", error);
+    console.error("Error fetching upcoming trips:s", error);
     throw error;
   }
 }
